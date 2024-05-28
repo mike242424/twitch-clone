7ecf3ab5b324b6e1457fc29a20f3f8681d67662f
Michael Byers <mikebyers24@gmail.com>
2024-05-28 11:29:32 -0400

    created login and register validation schemas using joi.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import Joi from 'joi';
+import { createValidator } from 'express-joi-validation';
+
 import { postLogin } from '../controllers/controllers.js';
 import { postRegister } from '../controllers/controllers.js';
 
+import { registerSchema } from '../validation/registerSchema.js';
+import { loginSchema } from '../validation/loginSchema.js';
+
 const router = express.Router();
 
-router.post('/register', postRegister);
+const validator = createValidator({});
 
-router.post('/login', postLogin);
+router.post('/register', validator.body(registerSchema), postRegister);
+
+router.post('/login', validator.body(loginSchema), postLogin);
 
 export default router;
