48a3bde2d44b9bc4fec1aa7c6fdef57d5be258ce
Michael Byers <mikebyers24@gmail.com>
2024-05-28 12:13:29 -0400

    created user model for db.

---
+++ server/src/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+});
+
+export default mongoose.model('User', userSchema);
