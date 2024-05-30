059d1ef4e705ccdfd8556f931ec4ed77cb5d3bb1
Michael Byers <mikebyers24@gmail.com>
2024-05-30 15:21:57 -0400

    get channels endpoint.

---
+++ server/src/controllers/channelControllers/getChannels.js
@@ -0,0 +1,26 @@
+import User from '../../models/User.js';
+
+export const getChannels = async (req, res) => {
+  try {
+    const users = await User.find({}, { username: 1, channel: 1 }).populate(
+      'channel',
+    );
+
+    const channels = users
+      // .filter((user) => user.channel.isActive) // todo
+      .map((user) => {
+        return {
+          id: user.channel._id,
+          title: user.channel.title,
+          avatarUrl: user.channel.avatarUrl,
+          username: user.username,
+          isOnline: false, // todo
+        };
+      });
+
+    return res.send({ channels });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ error: 'Error occured.' });
+  }
+};
--- server/src/routes/channelRoutes.js
+++ server/src/routes/channelRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { createValidator } from 'express-joi-validation';
 import { channelDetailsSchema } from '../validation/channelDetailsSchema.js';
 import { getChannelById } from '../controllers/channelControllers/getChannelById.js';
+import { getChannels } from '../controllers/channelControllers/getChannels.js';
 
 const router = express.Router();
 
 const validator = createValidator({});
 
+router.get('/', getChannels);
 router.get(
   '/:channelId',
   validator.params(channelDetailsSchema),
