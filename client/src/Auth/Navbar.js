419fde664c30987ee1694b060d69471e11bf502f
Michael Byers <mikebyers24@gmail.com>
2024-05-31 19:10:24 -0400

    navbar styling.

--- client/src/Auth/Navbar.js
+++ client/src/Auth/Navbar.js
@@ -3,7 +3,7 @@ import NavbarLogo from '../components/NavbarLogo';
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-slate-800 w-full p-4 px-6">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         <NavbarLogo />
         <h1 className="font-bold text-white text-2xl">Twitch Clone</h1>
       </div>
--- client/src/DashboardPage/Navbar.js
+++ client/src/DashboardPage/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center bg-slate-800 w-full p-4 px-6">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         <NavbarLogo />
         <h1 className="font-bold text-white text-2xl">Twitch Clone</h1>
       </div>
