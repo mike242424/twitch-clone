b795aa247f4b1180849b614c5337d793533580c1
Michael Byers <mikebyers24@gmail.com>
2024-06-01 08:39:22 -0400

    bold navbar buttons. added brose button to navbar.

--- client/src/DashboardPage/Navbar.js
+++ client/src/DashboardPage/Navbar.js
@@ -17,22 +17,30 @@ const Navbar = () => {
   }
 
   return (
-    <div className="flex justify-between items-center bg-slate-800 w-full p-4 px-6">
+    <div className="flex justify-between items-center bg-slate-800 w-full p-5 px-6">
       <div className="flex items-center gap-4">
         <NavbarLogo />
-        <h1 className="font-bold text-white text-2xl">Twitch Clone</h1>
+        <button className="font-bold text-white">Browse</button>
       </div>
 
       <div className="flex gap-4 text-white">
         {isAuthenticated ? (
           <>
-            <Link to={'/'}>Account</Link>
-            <button onClick={handleLogout}>Logout</button>
+            <Link className="font-bold" to={'/'}>
+              Account
+            </Link>
+            <button className="font-bold" onClick={handleLogout}>
+              Logout
+            </button>
           </>
         ) : (
           <>
-            <Link to={'/login'}>Login</Link>
-            <Link to={'/register'}>Register</Link>
+            <Link className="font-bold" to={'/login'}>
+              Login
+            </Link>
+            <Link className="font-bold" to={'/register'}>
+              Register
+            </Link>
           </>
         )}
       </div>
