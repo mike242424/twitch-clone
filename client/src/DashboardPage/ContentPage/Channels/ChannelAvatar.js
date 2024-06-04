0f34cb784e3d82bde9fe41d4da7fb698361254a0
Michael Byers <mikebyers24@gmail.com>
2024-06-04 13:47:06 -0400

    channel avatar === none styling.

--- client/src/DashboardPage/ContentPage/Channels/ChannelAvatar.js
+++ client/src/DashboardPage/ContentPage/Channels/ChannelAvatar.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 const ChannelAvatar = ({ channel, isLink, height, width }) => {
   const avatarContent =
     channel?.avatarUrl === 'none' ? (
-      <div className="bg-slate-500 w-16 h-16 rounded-full"></div>
+      <div
+        className={`${height ? 'h-10' : 'h-16'} ${
+          width ? 'w-10' : 'w-16'
+        } bg-slate-500 w-10 h-10 rounded-full`}
+      ></div>
     ) : (
       <img
         src={channel?.avatarUrl}
