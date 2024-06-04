65dbbc6cccc18f2dba0d10d5b2177021058056fc
Michael Byers <mikebyers24@gmail.com>
2024-06-04 10:13:30 -0400

    loading spinner settings page refactor.

--- client/src/DashboardPage/ContentPage/Settings/Settings.js
+++ client/src/DashboardPage/ContentPage/Settings/Settings.js
@@ -6,33 +6,41 @@ import StreamKey from './StreamKey';
 import LoadingSpinner from '../../../components/LoadingSpinner';
 
 const Settings = () => {
-  const { channelSettings, isLoading, getChannelSettings } =
+  const { channelSettings, isLoading, error, getChannelSettings } =
     useChannelSettings();
 
   useEffect(() => {
     getChannelSettings();
   }, []);
 
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    <div className="flex items-center justify-center min-h-screen">
+      {error}
+    </div>;
+  }
+
   return (
     <>
-      {!isLoading ? (
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-center font-bold text-3xl text-slate-800 my-8">
-            Account Settings
-          </h1>
-          <div className="flex flex-col items-center bg-slate-400 p-6 rounded-lg shadow-md mb-6 w-11/12 md:w-10/12 lg:w-9/12">
-            <SettingsForm channelSettings={channelSettings} />
-          </div>
-          <div className="flex flex-col items-center bg-slate-400 p-6 rounded-lg shadow-md mb-6 w-11/12 md:w-10/12 lg:w-9/12">
-            <ChangePasswordForm />
-          </div>
-          <StreamKey streamKey={channelSettings?.streamKey} />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-center font-bold text-3xl text-slate-800 my-8">
+          Account Settings
+        </h1>
+        <div className="flex flex-col items-center bg-slate-400 p-6 rounded-lg shadow-md mb-6 w-11/12 md:w-10/12 lg:w-9/12">
+          <SettingsForm channelSettings={channelSettings} />
         </div>
-      ) : (
-        <div className="flex items-center justify-center min-h-screen">
-          <LoadingSpinner />
+        <div className="flex flex-col items-center bg-slate-400 p-6 rounded-lg shadow-md mb-6 w-11/12 md:w-10/12 lg:w-9/12">
+          <ChangePasswordForm />
         </div>
-      )}
+        <StreamKey streamKey={channelSettings?.streamKey} />
+      </div>
     </>
   );
 };
