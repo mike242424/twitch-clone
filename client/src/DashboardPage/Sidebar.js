a46b78e77c1eb33f16f4ec5c23d9d8c42d09a87e
Michael Byers <mikebyers24@gmail.com>
2024-06-05 12:55:02 -0400

    conditionally renderi sidebar if logged in or not.

--- client/src/DashboardPage/Sidebar.js
+++ client/src/DashboardPage/Sidebar.js
@@ -2,40 +2,45 @@ import { useEffect } from 'react';
 import ChannelAvatar from './ContentPage/Channels/ChannelAvatar';
 import ChannelCard from './ContentPage/Channels/ChannelCard';
 import { useGetFollowedChannels } from '../hooks/useGetFollowedChannels';
+import { useUserDetails } from '../hooks/useUserDetails';
 
 const Sidebar = () => {
   const { followedChannels, getFollowedChannels } = useGetFollowedChannels();
-
-  console.log(followedChannels);
+  const { token } = useUserDetails();
 
   useEffect(() => {
     getFollowedChannels();
   }, []);
 
   return (
-    <div
-      className={`bg-slate-300 text-slate-800 hidden md:block sm:w-18 xl:w-64 p-2 xl:p-6 ${
-        followedChannels?.length > 0 ? '' : 'w-20'
-      } `}
-    >
-      <span className="hidden xl:block font-bold">FOLLOWED CHANNELS</span>
-      <div className="flex flex-col items-start mt-6">
-        {followedChannels?.map((channel) => (
-          <div key={channel._id} className="my-2">
-            <div className="xl:hidden">
-              <ChannelAvatar channel={channel} isLink={true} />
-            </div>
-            <div className="hidden xl:block">
-              <ChannelCard
-                channel={channel}
-                isLargeText={true}
-                isSmall={true}
-              />
-            </div>
+    <>
+      {token && (
+        <div
+          className={`bg-slate-300 text-slate-800 hidden md:block sm:w-18 xl:w-64 p-2 xl:p-6 ${
+            followedChannels?.length > 0 ? '' : 'w-20'
+          } `}
+        >
+          <span className="hidden xl:block font-bold">FOLLOWED CHANNELS</span>
+
+          <div className="flex flex-col items-start mt-6">
+            {followedChannels?.map((channel) => (
+              <div key={channel._id} className="my-2">
+                <div className="xl:hidden">
+                  <ChannelAvatar channel={channel} isLink={true} />
+                </div>
+                <div className="hidden xl:block">
+                  <ChannelCard
+                    channel={channel}
+                    isLargeText={true}
+                    isSmall={true}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
