9ab388a0ad7e2e228dcc4ecf604f926d6eb55392
Michael Byers <mikebyers24@gmail.com>
2024-08-12 11:23:14 -0400

    test locally

--- client/src/hooks/useGetFollowedChannels.js
+++ client/src/hooks/useGetFollowedChannels.js
@@ -13,13 +13,10 @@ export function useGetFollowedChannels() {
       setError('');
       setLoading(true);
       const response = await axios.get(
-        'https://twitch-clone-server.vercel.app/api/channels/followed',
+        'http://localhost:3002/api/channels/followed',
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-cache',
           },
         },
       );
