0f8aac0a043dc8b570f750e2d822facf605c42bb
Michael Byers <mikebyers24@gmail.com>
2024-08-13 07:27:01 -0400

    avatar loading bug.

--- client/src/DashboardPage/ContentPage/Channels/ChannelAvatar.js
+++ client/src/DashboardPage/ContentPage/Channels/ChannelAvatar.js
@@ -1,19 +1,45 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ChannelAvatar = ({ channel, isLink, isSmall }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    setIsError(true);
+    setIsLoading(false);
+  };
+
   const avatarContent =
-    channel?.avatarUrl === 'none' ? (
+    channel?.avatarUrl === 'none' || isError ? (
       <div
         className={`${
           isSmall ? 'h-10 w-10' : 'h-16 w-16'
         } bg-violet-500 rounded-full`}
       ></div>
     ) : (
-      <img
-        src={channel?.avatarUrl}
-        alt="user Avatar"
-        className={`${isSmall ? 'h-10 w-10' : 'h-16 w-16'}  rounded-full`}
-      />
+      <>
+        {isLoading && (
+          <div
+            className={`${
+              isSmall ? 'h-10 w-10' : 'h-16 w-16'
+            } bg-gray-200 rounded-full animate-pulse`}
+          ></div>
+        )}
+        <img
+          src={channel?.avatarUrl}
+          alt="user Avatar"
+          className={`${isSmall ? 'h-10 w-10' : 'h-16 w-16'} rounded-full ${
+            isLoading ? 'hidden' : 'block'
+          }`}
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+        />
+      </>
     );
 
   if (isLink) {
